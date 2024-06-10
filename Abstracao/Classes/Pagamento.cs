0ed58be540b272dcb02703cecbe0b5fdf6d5c039
João Vitor Alves Muniz <joao.vitor@akamaetech.com>
2024-06-09 21:18:06 -0300

    Conceito de abstracao

---
+++ Abstracao/Classes/Pagamento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracao.Classes
+{
+    public abstract class Pagamento
+    {
+        public abstract bool Pagar(double valor)
+        {
+           return ExecutarPagamento(valor);
+        }
+
+        private bool ExecutarPagamento(double valor) {
+
+            return true;
+        
+        }
+    }
+}
---
+++ Abstracao/Classes/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracao.Classes
+{
+    public  class Pessoa : Pagamento
+    {
+        public override bool Pagar(double valor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PagarBoleto()
+        {
+          
+        }
+    }
+}
